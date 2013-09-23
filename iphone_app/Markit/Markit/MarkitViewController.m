7535e8f93614b7dd93eef01b0010212f4e2919b3
iliasbeshimov <ilias@scopely.com>
2013-09-22 19:03:39 -0700

    iPhone app code.

--- iphone_app/Markit/Markit/MarkitViewController.m
+++ iphone_app/Markit/Markit/MarkitViewController.m
@@ -7,6 +7,12 @@
 //
 
 #import "MarkitViewController.h"
+#import "ASIS3BucketObject.h"
+
+static AmazonS3Client       *s3  = nil;
+static AmazonSimpleDBClient *sdb = nil;
+static AmazonSQSClient      *sqs = nil;
+static AmazonSNSClient      *sns = nil;
 
 @interface MarkitViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *label;
@@ -47,5 +53,67 @@
     }
     return YES;
 }
+
+
+// AWS S3 Connection Objects
+
++(AmazonS3Client *)s3
+{
+    [MarkitViewController validateCredentials];
+    return s3;
+}
+
++(AmazonSimpleDBClient *)sdb
+{
+    [MarkitViewController validateCredentials];
+    return sdb;
+}
+
++(AmazonSQSClient *)sqs
+{
+    [MarkitViewController validateCredentials];
+    return sqs;
+}
+
++(AmazonSNSClient *)sns
+{
+    [MarkitViewController validateCredentials];
+    return sns;
+}
+
++(bool)hasCredentials
+{
+    return (![ACCESS_KEY_ID isEqualToString:@"CHANGE ME"] && ![SECRET_KEY isEqualToString:@"CHANGE ME"]);
+}
+
++(void)validateCredentials
+{
+    if ((sdb == nil) || (s3 == nil) || (sqs == nil) || (sns == nil)) {
+        [MarkitViewController clearCredentials];
+        
+        s3  = [[AmazonS3Client alloc] initWithAccessKey:ACCESS_KEY_ID withSecretKey:SECRET_KEY];
+        s3.endpoint = [AmazonEndpoints s3Endpoint:US_WEST_2];
+        
+        sdb = [[AmazonSimpleDBClient alloc] initWithAccessKey:ACCESS_KEY_ID withSecretKey:SECRET_KEY];
+        sdb.endpoint = [AmazonEndpoints sdbEndpoint:US_WEST_2];
+        
+        sqs = [[AmazonSQSClient alloc] initWithAccessKey:ACCESS_KEY_ID withSecretKey:SECRET_KEY];
+        sqs.endpoint = [AmazonEndpoints sqsEndpoint:US_WEST_2];
+        
+        sns = [[AmazonSNSClient alloc] initWithAccessKey:ACCESS_KEY_ID withSecretKey:SECRET_KEY];
+        sns.endpoint = [AmazonEndpoints snsEndpoint:US_WEST_2];
+    }
+}
+
++(void)clearCredentials
+{
+    s3  = nil;
+    sdb = nil;
+    sqs = nil;
+    sns = nil;
+}
+
+
+
 
 @end
