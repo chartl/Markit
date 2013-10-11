ad4053d4a69dd986da2b90805a7a85900a96de6e
iliasbeshimov <ilias@scopely.com>
2013-10-11 14:50:21 -0700

    iPhone App

---
+++ iphone_app/Markit/Markit/Markit/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Markit
+//
+//  Created by Ilias Beshimov on 10/11/13.
+//  Copyright (c) 2013 Ilias Beshimov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iphone_app/Markit/Markit/Markit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Markit
+//
+//  Created by Ilias Beshimov on 10/11/13.
+//  Copyright (c) 2013 Ilias Beshimov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
